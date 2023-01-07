2ce41d510de0d4e357e1842c66370ca69997dc2c
Aridane Martín <aridanemartin@gmail.com>
2023-01-07 21:49:13 +0000

    Add correct font

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ export default function Document() {
           href="https://fonts.gstatic.com"
           crossOrigin="true"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
